94834426036cd8b153f7ebc70f09c31b9e895ffa
Karan Singh <godthatback21@gmail.com>
2025-05-26 12:53:00 +0530

    ui created

---
+++ src/cnnClassifier/pipeline/prediction.py
@@ -0,0 +1,34 @@
+import numpy as np
+import tensorflow as tf
+from keras.models import load_model
+from keras.utils import load_img, img_to_array
+
+import os
+
+
+
+class PredictionPipeline:
+    def __init__(self,filename):
+        self.filename =filename
+
+
+    
+    def predict(self):
+        ## load model
+        
+        model_path = os.path.join("artifacts","training", "model.h5")
+        model = load_model(model_path)
+
+        imagename = self.filename
+        test_image = load_img(imagename, target_size = (224,224))
+        test_image = img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis = 0)
+        result = np.argmax(model.predict(test_image), axis=1)
+        print(result)
+
+        if result[0] == 1:
+            prediction = 'Normal'
+            return [{ "image" : prediction}]
+        else:
+            prediction = 'Adenocarcinoma Cancer'
+            return [{ "image" : prediction}]
